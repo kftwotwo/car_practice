d27f802a972507fac768d426340e835406b92cb4
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-21 14:08:51 -0800

    add number 2 spec and pass

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,18 @@
 class Car
   define_method(:initialize) do |make, model, year|
     @year = year
+    @make = make
   end
 
   define_method(:age) do
     current_year = Time.new().year()
     current_year - (@year)
+  end
+
+  define_method(:worth_buying?) do
+    american_makes = ["Chrysler", "Ford", "GM"]
+    american = american_makes.include?(@make)
+    new_enough = self.age().<=(15)
+    american. & (new_enough)
   end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,9 +3,15 @@ require('car')
 
 describe(Car) do
   describe("#age") do
-    it("returns the vehicles age") do
-      test_vehicle = Car.new("Toyota", "Prius", 2000)
-      expect(test_vehicle.age()).to(eq(16))
+    it "returns the cars age" do
+      test_car = Car.new("Toyota", "4Runnner", 2000)
+      expect(test_car.age()).to(eq(16))
+    end
+  end
+  describe("#worth_buying?") do
+    it "returns false if the vehicle is not american made and younger than 17 years old" do
+      test_car = Car.new("Toyota", "4Runner", 2000)
+      expect(test_car.worth_buying?()).to(eq(false))
     end
   end
 end
